5b874df7518adbf92925baa82ff8017323aeefe0
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-05-22 20:57:05 +0100

    Further tidying.

--- smg/rotorcontrol/controllers/aws_transcribe_drone_controller.py
+++ smg/rotorcontrol/controllers/aws_transcribe_drone_controller.py
@@ -8,11 +8,11 @@ import sounddevice
 import threading
 
 from queue import Queue
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Set, Tuple
 
 from amazon_transcribe.client import TranscribeStreamingClient
 from amazon_transcribe.handlers import TranscriptResultStream, TranscriptResultStreamHandler
-from amazon_transcribe.model import TranscriptEvent
+from amazon_transcribe.model import Result, TranscriptEvent
 
 from smg.rotory.drones import Drone
 
@@ -25,17 +25,17 @@ class AWSTranscribeDroneController(DroneController):
     # NESTED TYPES
 
     class StreamHandler(TranscriptResultStreamHandler):
-        """TODO"""
+        """A handler that can handle transcript events from a stream processed by AWS Transcribe."""
 
         # CONSTRUCTOR
 
         def __init__(self, stream: TranscriptResultStream, command_queue: "Queue[str]", *, debug: bool = True):
             """
-            TODO
+            Construct a handler that can handle transcript events from a stream processed by AWS Transcribe.
 
-            :param stream:          TODO
-            :param command_queue:   TODO
-            :param debug:           TODO
+            :param stream:          The stream.
+            :param command_queue:   The queue onto which to push any recognised drone commands.
+            :param debug:           Whether or not to print out debug messages.
             """
             super().__init__(stream)
 
@@ -46,47 +46,38 @@ class AWSTranscribeDroneController(DroneController):
 
         async def handle_transcript_event(self, transcript_event: TranscriptEvent) -> None:
             """
-            TODO
+            Handle a transcript event from a stream processed by AWS Transcribe.
 
-            :param transcript_event:    TODO
+            :param transcript_event:    The event.
             """
-            # TODO
-            # This handler can be implemented to handle transcriptions as needed.
-            # Here's an example to get started.
-            results = transcript_event.transcript.results
+            # Get a list of transcription results, each of which corresponds to a portion of the input audio stream.
+            results: List[Result] = transcript_event.transcript.results
 
-            # Only print final result when is_partial is FALSE to get final transcript result without duplicates.
+            # For each transcription result:
             for result in results:
+                # For each possible transcription of the corresponding portion of the input audio stream:
                 for alt in result.alternatives:
-                    if result.is_partial:
-                        partial_result = alt.transcript
-                        partial_result = partial_result.lower()  # Convert result to case-insensitive.
-                        partial_result = "".join(filter(lambda c: str.isalnum(c) or str.isspace(c), partial_result))
-                        print("Transcription: ", partial_result)
-                        # print(result.start_time, result.end_time)
-                        compute_time = result.end_time - result.start_time
-                        print("Compute time (sec): ", round(compute_time, 3))  # Transcription compute time.
+                    # Get the transcription, convert it to lower case, and then remove any character that is neither
+                    # alphanumeric nor whitespace.
+                    transcript: str = alt.transcript
+                    transcript = transcript.lower()
+                    transcript = "".join(filter(lambda c: str.isalnum(c) or str.isspace(c), transcript))
 
-                        voice_to_command: Dict[str, str] = {
-                            "back": "back",
-                            "backward": "backward",
-                            "down": "down",
-                            "forward": "forward",
-                            "land": "land",
-                            "level": "level",
-                            "move left": "move left",
-                            "move right": "move right",
-                            "rotate left": "rotate left",
-                            "rotate right": "rotate right",
-                            "stop": "stop",
-                            "straight": "straight",
-                            "take off": "take off",
-                            "up": "up"
-                        }
+                    # If we're debugging, print out both the transcription and the time it took to compute it.
+                    if self.__debug:
+                        print("Transcription: ", transcript)
+                        compute_time: float = result.end_time - result.start_time
+                        print("Compute Time (s): ", round(compute_time, 3))
 
-                        for voice, command in voice_to_command.items():
-                            if voice in partial_result:
-                                self.__command_queue.put(command)
+                    # Add any relevant drone commands that have been found in the audio stream to the command queue.
+                    possible_commands: Set[str] = {
+                        "back", "backward", "down",  "forward", "land", "level", "move left", "move right",
+                        "rotate left", "rotate right", "stop", "straight", "take off", "up"
+                    }
+
+                    for command in possible_commands:
+                        if command in transcript:
+                            self.__command_queue.put(command)
 
     # CONSTRUCTOR
 
